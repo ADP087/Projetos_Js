984b26ce8d23fe37a0c09cd5d930b95ea422a9c4
ADP <arthur.passarelli087@gmail.com>
2025-03-15 01:02:13 -0300

    Terminando 1 dos projetos
    Terminei o projeto click e começando a estrutura do projeto IMC

--- Projetos/Click/script.js
+++ Projetos/Click/script.js
@@ -1,6 +1,35 @@
 let contador = 0;
-const resultado = document.querySelector('.numero')
+const resultado = document.querySelector('.numero');
 
-resultado.addEventListener('', function(){
-    //Vamos parar por aqui hj
+function criaP(texto){
+    const p = document.createElement('p');
+    p.textContent = texto
+
+    return p;
+}
+
+function mensagem(){
+    const p = criaP(contador);
+
+    resultado.appendChild(p);
+}
+
+// ==== Botão MAIS
+const mais = document.querySelector('#mais');
+
+mais.addEventListener('click', function(){
+    contador++;
+
+    resultado.textContent = contador + ' Clicks';
 });
+
+// ==== Botão RESET
+const resetar = document.querySelector('#reset');
+
+resetar.addEventListener('click', function(){
+    contador = 0;
+
+    resultado.textContent = 0;
+});
+
+mensagem();
