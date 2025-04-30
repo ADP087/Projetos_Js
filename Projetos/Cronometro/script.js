ad8ec007622b8c00572bbaf0a0479e025b52e9c4
ADP <139926119+ADP087@users.noreply.github.com>
2025-04-30 12:05:36 -0300

    Function terminada no Cronômetro
    Function terminada no cronômetro, falta só mexer com style

--- Projetos/Cronometro/script.js
+++ Projetos/Cronometro/script.js
@@ -2,3 +2,61 @@ const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
+
+let horas = 0;
+let min = 0;
+let seg = 0;
+let ms = 0;
+
+let timer;
+
+function iniciarTimer() {
+    if (!timer) {
+        timer = setInterval(function () {
+            ms++;
+
+            if (ms >= 100) {
+                ms = 0;
+                seg++;
+            }
+            if (seg >= 60) {
+                seg = 0;
+                min++;
+            }
+            if (min >= 60) {
+                min = 0;
+                horas++;
+            }
+
+            relogio.style.color = 'black';
+
+            relogio.innerHTML = (horas > 0 ? `${horas.toString().padStart(2, '0')}:` : '') + `${min.toString().padStart(2, '0')}:${seg.toString().padStart(2, '0')}<span>:${ms.toString().padStart(2, '0')}</span>`;
+        }, 10);
+    }
+}
+
+//==== Funções do cronômetro ====//
+document.addEventListener('click', function (e) {
+    const elemento = e.target;
+
+    if (elemento.classList.contains('iniciar')) {
+        iniciarTimer();
+    }
+
+    if (elemento.classList.contains('pausar')) {
+        clearInterval(timer);
+        timer = null;
+        relogio.style.color = 'red';
+    }
+
+    if (elemento.classList.contains('zerar')) {
+        clearInterval(timer);
+        horas = 0;
+        min = 0;
+        seg = 0;
+        ms = 0;
+        timer = null;
+        relogio.style.color = 'black';
+        relogio.innerHTML = '00:00<span>:00</span>';
+    }
+});
